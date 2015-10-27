9920923a6e55b753224cf4b2fcea4117454024e9
lexaxa <alexoheah@gmail.com>
2015-10-27 17:38:51 +0300

    add navigation drawer

--- app/src/main/java/com/alexis/pomodoro/WhatsHotFragment.java
+++ app/src/main/java/com/alexis/pomodoro/WhatsHotFragment.java
@@ -1,7 +1,6 @@
 package com.alexis.pomodoro;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +14,7 @@ public class WhatsHotFragment extends android.app.Fragment {
     public WhatsHotFragment() {
     }
 
-    @Nullable
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_whatshot, container, false);
--- app/src/main/java/com/alexis/pomodoro/test.java
+++ app/src/main/java/com/alexis/pomodoro/test.java
@@ -1,27 +1,31 @@
 package com.alexis.pomodoro;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class test extends AppCompatActivity {
 
+    public static final int MAIN_LAYOUT = R.layout.activity_test;
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(MAIN_LAYOUT);
 
         initToolbar();
+        initNavigationView();
     }
 
-
-
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
 
     private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
